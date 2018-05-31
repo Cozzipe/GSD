65c6583ba195216a69d4eafc8bb688072294fbac
Giovanni Maltisotto <giovannijmaltisotto@gmail.com>
2018-05-31 23:50:21 +0200

    CAROSELLO in home.html

--- GSD/script.js
+++ GSD/script.js
@@ -12,3 +12,42 @@
   }
 }
 */
+
+var flag=1;
+
+function carousel()
+{
+  img1 = document.getElementById("carosello1");
+  img2 = document.getElementById("carosello2");
+
+  switch (flag)
+  {
+    case 1:
+      img1.style.display="block";
+      img2.style.display="none";
+      break;
+
+    case 2:
+      img1.style.display="none";
+      img2.style.display="block";
+      break;
+  }
+}
+
+function piuCarosello()
+{
+  if(flag<2)
+  {
+    flag=flag+1;
+  }
+  carousel();
+}
+
+function menoCarosello()
+{
+  if(flag>1)
+  {
+    flag=flag-1;
+  }
+  carousel();
+}
